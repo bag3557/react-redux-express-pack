995f5d3bf2cf40b0d71124128506ba093ab981f7
ajitbhaik <ajit.bhaik@figmd.in>
2019-04-04 12:26:28 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+import logo from './logo.svg'
+import './App.css'
+import store from './store'
+import Customers from './components/Customer/customers'
+
+class App extends Component {
+
+  render () {
+    return (
+      <Provider store={ store }>
+        <div className="App">
+          <header className="App-header">
+            <img src={ logo } className="App-logo" alt="logo"/>
+            <h1 className="App-title">React/Redux Express Starter</h1>
+          </header>
+          <Customers/>
+        </div>
+      </Provider>
+    )
+  }
+}
+
+export default App
